2acba3ae872425b7eb47002e20521bbcfa42b099
BagoesPantera <bagoespantera01@gmail.com>
2022-10-02 09:02:00 +0800

    find one, create and update

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,30 @@ class TodosController < ApplicationController
         @todos = Todo.all
         render json: @todos
     end    
+
+    def show
+        @todo = Todo.find(params[:id])
+        render json: @todo
+    end
+
+    def create
+        @todo = Todo.new(params.require(:todo).permit(:title, :description))
+
+        if @todo.save
+            render json: @todo
+        else 
+            render json: @todo.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+
+        if @todo.update(params.require(:todo).permit(:title, :description))
+            render json: @todo
+        else 
+            render json: @todo.errors, status: :unprocessable_entity
+        end
+
+    end
 end
